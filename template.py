47c454a4ded14f3f5c4889c8b2406f777525a94a
wgorick <wgorick@yext.com>
2024-02-05 18:12:54 -0700

    add template file

---
+++ template.py
@@ -0,0 +1,19 @@
+"""
+Function description
+
+Time Complexity:
+Space Complexity:
+
+"""
+
+class Solution:
+  def function_name(self, arr):
+    pass
+
+
+def main():
+  sol = Solution()
+  print(sol.function_name([]))
+  
+
+main()
