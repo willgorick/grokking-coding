256f6ed2d8e3b7a5cb8536ea5c69ae8e9610b204
willgorick <wig4da@virginia.edu>
2024-02-08 12:52:15 -0700

    add longest substring with k distinct

--- sliding_window/medium/1_longest_substring_with_k_distinct_characters.py
+++ sliding_window/medium/1_longest_substring_with_k_distinct_characters.py
@@ -2,19 +2,38 @@
 Given a string, find the length of the longest substring in it with no more than K distinct characters.
 You can assume that K is less than or equal to the length of the given string.
 
-Time Complexity:
-Space Complexity:
+Time Complexity: O(n) - The outer for loop looks at each character once, and the inner while loop looks at each character at most once
+Space Complexity: O(k) for storing k+1 characters in our defaultdict at most
 
 """
+from collections import defaultdict
 
 class Solution:
-  def function_name(self, arr):
-    pass
+  def longest_substring(self, str1, k):
+    max_length, start = 0, 0
+    curr_letters = defaultdict(int)
+
+    for end in range(len(str1)):
+      curr_letters[str1[end]] += 1
+      if len(curr_letters) <= k:
+        max_length = max(max_length, end-start+1)
+      else:
+        while len(curr_letters) > k:
+          start_letter = str1[start]
+          curr_letters[start_letter] -= 1
+          if curr_letters[start_letter] == 0:
+            del curr_letters[start_letter]
+          start += 1
+      
+    return max_length
+
 
 
 def main():
   sol = Solution()
-  print(sol.function_name([]))
+  print(sol.longest_substring(str1="araaci", k=2))
+  print(sol.longest_substring("araaci", 1))
+  print(sol.longest_substring("cbbebi", 3))
   
 
 main()
